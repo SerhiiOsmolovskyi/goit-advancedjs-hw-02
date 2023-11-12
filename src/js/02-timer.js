e2c1aa87dfa1952507cdda29228c42f6eb017393
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2023-11-12 15:50:25 -0700

    Homework 2 First Attempt

---
+++ src/js/02-timer.js
@@ -0,0 +1,117 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+let datetimePicker;
+
+document.addEventListener('DOMContentLoaded', function () {
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      const selectedDate = selectedDates[0];
+      const currentDate = new Date();
+
+      if (selectedDate <= currentDate) {
+        iziToast.error({
+          title: 'Error',
+          message: 'Please choose a date in the future',
+        });
+        document.querySelector('button[data-start]').disabled = true;
+      } else {
+        document.querySelector('button[data-start]').disabled = false;
+      }
+    },
+  };
+
+  datetimePicker = flatpickr("#datetime-picker", options);
+
+  document.querySelector("#datetime-picker").addEventListener('click', () => {
+    datetimePicker.open();
+  });
+
+  document.querySelector('button[data-start]').addEventListener('click', () => {
+    const selectedDate = datetimePicker.selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+    } else {
+      startTimer(selectedDate);
+    }
+  });
+
+  const timerContainer = document.querySelector('.timer');
+  const fields = document.querySelectorAll('.field');
+
+  timerContainer.style.display = 'flex';
+  timerContainer.style.justifyContent = 'space-between';
+  timerContainer.style.width = '30%';
+
+  fields.forEach(field => {
+    field.style.textAlign = 'center';
+    field.style.flex = '1';
+    field.style.display = 'flex';
+    field.style.flexDirection = 'column';
+    field.style.justifyContent = 'flex-start';
+    field.style.gap = '5px';
+  });
+
+  const values = document.querySelectorAll('.value');
+  const labels = document.querySelectorAll('.label');
+
+  values.forEach(value => {
+    value.style.fontSize = '2em';
+    value.style.fontWeight = 'bold';
+  });
+
+  labels.forEach(label => {
+    label.style.fontSize = '0.8em';
+    label.style.color = '#555';
+  });
+
+  function startTimer(endDate) {
+    const interval = setInterval(() => {
+      const timeDifference = endDate - new Date();
+
+      if (timeDifference < 0) {
+        clearInterval(interval);
+        iziToast.success({
+          title: 'Success',
+          message: 'Countdown timer reached zero!',
+        });
+      } else {
+        const time = convertMs(timeDifference);
+
+        document.querySelector('[data-days]').textContent = addLeadingZero(time.days);
+        document.querySelector('[data-hours]').textContent = addLeadingZero(time.hours);
+        document.querySelector('[data-minutes]').textContent = addLeadingZero(time.minutes);
+        document.querySelector('[data-seconds]').textContent = addLeadingZero(time.seconds);
+      }
+    }, 1000);
+  }
+
+  function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+
+  function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+  }
+});
---
+++ src/js/03-promises.js
@@ -0,0 +1,51 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const delayInput = document.querySelector('input[name="delay"]');
+    const stepInput = document.querySelector('input[name="step"]');
+    const amountInput = document.querySelector('input[name="amount"]');
+
+    const firstDelay = parseInt(delayInput.value);
+    const step = parseInt(stepInput.value);
+    const amount = parseInt(amountInput.value);
+
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, firstDelay + (i - 1) * step)
+        .then(({ position, delay }) => {
+          iziToast.success({
+            title: 'Fulfilled',
+            message: `Promise ${position} fulfilled in ${delay}ms`,
+            position: 'topRight',
+          });
+        })
+        .catch(({ position, delay }) => {
+          iziToast.error({
+            title: 'Rejected',
+            message: `Promise ${position} rejected in ${delay}ms`,
+            position: 'topRight',
+            });
+        });
+    }
+  });
+
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+});
